5371ae779df7a80c169b7c8dbcf6df47c6b998f7
Donovan Gallaway <donovangallaway@gmail.com>
2021-11-16 10:01:44 -0800

    basic backend

---
+++ server.js
@@ -0,0 +1,42 @@
+//////////////////////////
+// Dependencies
+//////////////////////////
+
+ require('dotenv').config()
+ const express = require('express')
+ const morgan = require('morgan')
+ const mongoose = require('./db/db')
+ const cors = require('cors')
+ const app = express()
+ const AuthRouter = require('./controllers/user')
+ const auth = './auth/index.js'
+
+ const {PORT,DATABASE_URL} = process.env
+
+ 
+ module.exports = mongoose
+
+
+ //////////////////////////
+ // Middleware
+ //////////////////////////
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+
+///////////////////////////
+// Routes
+///////////////////////////
+
+app.get('/', auth, (req,res)=>{
+    res.json(req.payload)
+})
+
+
+//Routers
+app.use('/auth', AuthRouter)
+
+// Listener
+app.listen(PORT, () => {console.log('listening on port', PORT)})
