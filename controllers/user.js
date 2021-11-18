734269ffabeee36ebbb579a89aa3cb69702f7441
Donovan Gallaway <donovangallaway@gmail.com>
2021-11-17 16:00:04 -0800

    auth fully tested and functional

--- controllers/user.js
+++ controllers/user.js
@@ -20,8 +20,9 @@ router.post('/signup', async (req,res) => {
 
 router.post('/login', async (req,res) => {
     try{
-        const {username, password} = req.body
-        const user = await User.find({username})
+        const username = req.body.username
+        const password= req.body.password
+        const user = await User.findOne({username})
         if (user){
             const match = await bcrypt.compare(password, user.password)
             if(match){
@@ -34,7 +35,7 @@ router.post('/login', async (req,res) => {
             res.status(400).json({error: "user does not exist"})
         }
     } catch(error){
-        res.status(400).json({error})
+        res.status(400).json(error)
     }
 })
 
